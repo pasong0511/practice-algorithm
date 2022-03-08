4e886fa04d332de0622a35738863157fe019d55e
pasong0511 <86876414+pasong0511@users.noreply.github.com>
2022-03-08 14:11:36 +0900

    [Silver II] Title: 좌표 압축, Time: 1964 ms, Memory: 169752 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -6,12 +6,12 @@ num_list = list(map(int, sys.stdin.readline().rstrip().split()))
 dic_num = {}
 result_list = []
 
-num_set = set(num_list)         #중복제거
-new_list = list(num_set)        #중복 제거된 숫자 리스트
-new_list.sort()                 #정렬하기
+num_set = set(num_list)             #중복제거
+none_overlap = list(num_set)        #중복 제거된 숫자 리스트 생성
+none_overlap.sort()                 #중복 제거된 리스트 정렬하기
 
-for i in range(len(new_list)) : 
-    dic_num[new_list[i]] = i    #딕셔너리에 {숫자 값 : 정렬된 순서} 저장                  
+for i in range(len(none_overlap)) :
+    dic_num[none_overlap[i]] = i    #딕셔너리에 {숫자 값 : 정렬된 순서} 저장                  
 
-for i in range(len(num_list)) :      #num_list에 순서에 맞게 순서 result_list에 추가
-    print(dic_num[num_list[i]], end=" ")
+for i in range(len(num_list)) :             #딕셔너리에 숫자에 대한 정렬된 순서 저장되어 있으니
+    print(dic_num[num_list[i]], end=" ")    #num_list에 맞게 출력
