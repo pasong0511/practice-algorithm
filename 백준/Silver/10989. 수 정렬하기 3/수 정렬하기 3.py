8ee35f4322aa828c215c338b6a09404af580c662
pasong0511 <86876414+pasong0511@users.noreply.github.com>
2022-03-08 12:03:50 +0900

    [Silver V] Title: 수 정렬하기 3, Time: 9712 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Silver/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(input())
+
+check_list = [0] * 10001
+
+for i in range(n) :
+    input_num = int(sys.stdin.readline())
+
+    check_list[input_num] = check_list[input_num] + 1
+
+for i in range(10001) :
+    if check_list != 0 :
+        for j in range(check_list[i]) :
+            print(i)
