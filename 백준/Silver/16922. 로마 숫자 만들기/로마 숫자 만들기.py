686768a764db961bc7086f50bb6fef4695d68274
pasong0511 <86876414+pasong0511@users.noreply.github.com>
2022-03-10 01:35:42 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
@@ -1,14 +1,23 @@
-from itertools import combinations_with_replacement
+import sys
 
 n = int(input())
 num_list = [1, 5, 10, 50]
+com_list = []
+new_com_list = []
 result = []
-#중복조합 리스트 : combinations_with_replacement([반복가능한 개체], 길이)
-#[1, 5, 10, 50]에 있는 숫자를 n개 뽑는다(중복 가능)
-com_list = list(combinations_with_replacement(num_list, n))
 
-#중복조합으로 n개 뽑은 종류 리스트를 더해준다.
-for i in com_list :
+def DFS(L, start_index) :
+    if L == n :
+        new_com_list.append(com_list[:])
+        return
+    else :
+        for i in range(start_index, len(num_list)) :
+            com_list.append(num_list[i])
+            DFS(L+1, i)
+            com_list.pop()
+
+DFS(0, 0)
+for i in new_com_list :
     result.append(sum(i))
 
-print(len(set(result)))     #더한 값이 중복값일 수 있어서 set이용
+print(len(set(result)))
