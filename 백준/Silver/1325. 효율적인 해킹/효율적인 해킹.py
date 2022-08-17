c868b46495e2d05d6cb338a04eebd96e0377703c
PARK SONG HEE <86876414+pasong0511@users.noreply.github.com>
2022-08-17 15:30:48 +0900

    [Silver I] Title: 효율적인 해킹, Time: 11028 ms, Memory: 215444 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -3,36 +3,42 @@ from collections import deque
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
-countList = [0] * (n+1)
-
-def BFS(node) :
-    dq = deque()
-    dq.append(node)
-
-    visited = [False] * (n + 1)
-    visited[node] = True
-    #큐에 있는거 하나씩 빼면서 BFS 탐구
-    while dq :
-        nowNode = dq.popleft()
-
-        for nextNode in graph[nowNode] :
-            if visited[nextNode] is True :
-                continue
-            else :
-                dq.append(nextNode)
-                visited[nextNode] = True
-    return visited.count(True)
-
+countList = [0 for _ in range(n + 1)]
 
 for i in range(m) :
     dstn, src = map(int, input().split())
     graph[src].append(dstn)
 
-for node in range(1, n + 1) :
-    countList[node] = BFS(node)
+def BFS(startNode) :
+    dq = deque()
+    visited = [False] * (n + 1)
+    visited[startNode] = True
+    dq.append(startNode)
+    count = 0
+    #print("시작노드", startNode)
+    
+    #큐가 빌때까지
+    while dq :
+        nowNode = dq.popleft()          #팝하면서 방문한다
+        count += 1
+        #print("팝", nowNode)
 
-maxNum = max(countList)
+        for nextNode in graph[nowNode] :
+            if visited[nextNode] is True :
+                continue
+            else :
+                dq.append(nextNode)     #다음 방문할 노드 큐에 넣기
+                visited[nextNode] = True
+    return count
 
-for i in range(len(countList)) :
-    if countList[i] == maxNum :
+#시작점 하나씩 뽑아서 노드 돌기
+#시작점은 1에서 5까지 이다.
+for startNode in range(1, n+1) :
+    countList[startNode] = BFS(startNode)
+
+#print(countList)
+maxCount = max(countList)
+
+for i in range(1, n+1) :
+    if countList[i] == maxCount :
         print(i, end=" ")
